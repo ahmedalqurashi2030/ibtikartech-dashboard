a7643f553692cd1f9b8021bed57cefc4f04e8595
ahmedalqurashi2030 <MATJARTECH.SA@GMAIL.COM>
2026-08-13 06:09:13 +0300

    dev: include support schema in Codespaces bootstrap

--- scripts/codespaces-bootstrap.sh
+++ scripts/codespaces-bootstrap.sh
@@ -5,7 +5,7 @@ python -m pip install --upgrade pip
 pip install -e ".[dev]"
 
 python manage.py makemigrations \
-  accounts core crm services sales customer_portal projects content --noinput
+  accounts core crm services sales customer_portal projects support content --noinput
 python manage.py migrate --noinput
 python manage.py check
 
