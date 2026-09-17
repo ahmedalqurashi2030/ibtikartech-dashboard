995c1fdab9c4d1066e6bccee1ecb206d61d9a4ed
ahmedalqurashi2030 <MATJARTECH.SA@GMAIL.COM>
2026-09-18 02:14:54 +0300

    Expose approved SEO breadcrumbs to public templates

--- apps/content/context_processors.py
+++ apps/content/context_processors.py
@@ -1,3 +1,5 @@
+from apps.core.seo_context import build_seo_breadcrumbs
+
 from .site_config import build_runtime_config, resolve_site_config
 
 
@@ -6,4 +8,5 @@ def site_configuration(request):
     return {
         "site_config": site_config,
         "runtime_config": build_runtime_config(request, site_config),
+        "seo_breadcrumbs": build_seo_breadcrumbs(request),
     }
